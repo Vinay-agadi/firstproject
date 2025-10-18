b68b3d7bb8daa9755241cba946b97156c2cbd07b
“Vinay-agadi <vinaykumaragadi@gmail.com>
2025-10-18 17:31:44 +0530

    loginpage

---
+++ src/test/java/login_vinay/Login_page.java
@@ -0,0 +1,13 @@
+package login_vinay;
+
+import org.testng.annotations.Test;
+
+public class Login_page 
+{
+	@Test
+	public void login()
+	{
+		System.out.println("login page should be displayed");
+	}
+
+}
